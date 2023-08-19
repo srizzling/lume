28e730ff4241e83cebabd5693998ae619b432dcb
Sriram Venkatesh <srizzling@users.noreply.github.com>
2023-08-19 22:13:15 +1000

    fix: again

--- functions/lume.fish
+++ functions/lume.fish
@@ -1,80 +1,77 @@
 function lume
-    set -l hide_additional false
-    set -l jq_filter ""
-    set -l dot_notation false
+    # Define flags and arguments with argparse
+    argparse "H/hide_additional_fields" "f/flat_fields" "d/filter_string=" -- $argv
 
-    argparse "h/help" "H/hide-additional" "f/jq-filter=" "D/dot-notation" -- $argv
+    # Determine maximum line count from environment variable or default to 10
+    set max_line_count (or (env LUME_LINE_COUNT) 10)
+    set line_count 0
 
-    if set -q _flag_help
-        echo "Usage: lume [OPTIONS]"
-        echo ""
-        echo "A human-friendly structured log viewer."
-        echo ""
-        echo "Options:"
-        echo "  -h, --help                Show this help message and exit."
-        echo "  -H, --hide-additional     Hide additional fields in the log."
-        echo "  -f, --jq-filter=FILTER    Provide a jq filter to extract specific fields."
-        echo "  -D, --dot-notation        Transform nested fields to dot notation."
-        echo ""
-        echo "Examples:"
-        echo "  lume -H"
-        echo "  lume -f '.details' -D"
-        return
-    end
-
-    if set -q _flag_hide_additional; set hide_additional true; end
-    if set -q _flag_jq_filter; set jq_filter $_flag_jq_filter; end
-    if set -q _flag_dot_notation; set dot_notation true; end
-
-    set -l line_count 0
-    if set -q LUME_LINE_COUNT
-        set max_line_count $LUME_LINE_COUNT
-    else
-        set max_line_count 10
-    end
-
+    # Process each incoming log line
     while read -l line
-        set line_count (math $line_count+1)
+        # Extract main fields from the log line
+        set log_level (echo $line | jq -r '.lvl // .level // "UNKNOWN"')
+        set message (echo $line | jq -r '.msg // .message // "No Message"')
+        set timestamp (echo $line | jq -r '.timestamp // .time // ""')
+        set human_timestamp (date -d $timestamp +'%b %d %H:%M:%S' 2>/dev/null; or echo "Invalid Timestamp")
 
-        set -l log_level (echo $line | jq -r ".lvl? // .level? // empty")
-        set -l message (echo $line | jq -r ".msg? // .message? // empty")
-        set -l timestamp (echo $line | jq -r ".timestamp? // .time? // empty")
-        set -l human_timestamp (date --date=$timestamp +"%b %d %H:%M:%S")
+        set additional_fields ""
 
+        # Prepare the additional fields if they are not to be hidden
+        if not $_flag_hide_additional_fields
+            set additional_fields (echo $line | jq 'del(.lvl, .level, .msg, .message, .timestamp, .time)')
+            # Flatten the additional fields if the flat_fields flag is set
+            if $_flag_flat_fields
+                set additional_fields (echo $additional_fields | jq -r 'recurse | to_entries[] | "\(.key): \(.value)"')
+            end
+        end
+
+        # Apply filtering if a filter string is provided
+        if test -n "$_flag_filter_string"
+            set additional_fields (echo $additional_fields | jq "$_flag_filter_string")
+        end
+
+        # Begin printing the processed log line
+        echo -n "[ "
+
+        # Colorize log level based on its value
         switch $log_level
             case "INFO" "info"
-                set log_level_color (set_color green)
-            case "WARN" "warning" "WARNING"
-                set log_level_color (set_color yellow)
-            case "ERROR" "error" "ERR"
-                set log_level_color (set_color red)
+                set_color green
+                echo -n "INFO"
+            case "ERROR" "error"
+                set_color red
+                echo -n "ERROR"
+            case "WARN" "warn"
+                set_color yellow
+                echo -n "WARN"
             case "TRACE" "trace"
-                set log_level_color (set_color blue)
-            case "*"
-                set log_level_color (set_color white)
+                set_color blue
+                echo -n "TRACE"
+            case '*'
+                set_color normal
+                echo -n "UNKNOWN"
         end
 
-        # Additional Fields Processing
-        set additional_fields ""
-        if not $hide_additional
-            set json_fields (echo $line | jq -c "del(.lvl, .level, .msg, .message, .timestamp, .time)")
+        # Reset color and print the rest of the log line
+        set_color normal
+        echo " ] - $human_timestamp - $message - $additional_fields"
 
-            if test "$jq_filter" != ""
-                set json_fields (echo $json_fields | jq -c $jq_filter)
-            end
-
-            if $dot_notation
-                set json_fields (echo $json_fields | jq -c -r 'to_entries[] | "\(.key): \(.value)"')
-            end
-
-            set additional_fields " - $json_fields"
-        end
-
-        echo "$log_level_color\[ $log_level \](set_color normal) - $human_timestamp - $message$additional_fields"
-
+        # Count processed lines and give a warning if the threshold is reached
+        set line_count (math $line_count+1)
         if test $line_count -ge $max_line_count
-            echo (set_color yellow)"[ WARN from lume ] Accumulated $line_count lines without a recognizable log level. Consider adjusting LUME_LINE_COUNT if seeing this frequently."(set_color normal)
+            set_color yellow
+            echo "[ WARN from lume ] Exceeded $max_line_count lines of processed logs. Accumulation might affect performance."
+            set_color normal
             set line_count 0
         end
     end
+end
+
+function lume_help
+    echo "Usage: lume [OPTIONS]"
+    echo ""
+    echo "Options:"
+    echo "  -H, --hide_additional_fields  Hide the additional fields in the log output"
+    echo "  -f, --flat_fields             Display additional fields in a flattened manner"
+    echo "  -d, --filter_string=STRING    Filter which fields to display using a jq filter string"
 end
